ca94327bf81008325c52b1d412c3db7adbdcb1eb
CasperLarsen-cmyk <casper.larsen.a@gmail.com>
2026-05-18 15:10:03 +0200

    1

--- Assets/Scripts/Normalize.cs
+++ Assets/Scripts/Normalize.cs
@@ -14,7 +14,7 @@ public class Normalize : MonoBehaviour
         print(NormalizeInPlace(ref vec1));
         Vector3 vec2 = vec;
 
-        tmPro.text = "1: " + vec1 + "\n2: " + vec2;
+        tmPro.text = "1: " + vec1 + "\n x: " + vec1.x + "\n y: " + vec1.y + "\n z: " + vec1.z + "\n2: " + vec2;
     }
 
     // Update is called once per frame
@@ -23,13 +23,14 @@ public class Normalize : MonoBehaviour
 
     }
 
-    float NormalizeInPlace(ref Vector3 v)
+    Vector3 NormalizeInPlace(ref Vector3 v)
     {
-        float length = v.magnitude; // + 1e-10f
+        float length = v.magnitude + 1e-10f;
         float invLength = 1 / length;
         v.x = v.x * invLength;
         v.y = v.y * invLength;
         v.z = v.z * invLength;
-        return length * invLength;
+        return v;
+        //return new(v.x * invLength, v.y * invLength, v.z * invLength);
     }
 }
